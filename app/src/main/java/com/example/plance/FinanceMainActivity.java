4f6905fc2a8f49c691da1c53937878f00ac4f121
rayhanwb <rayhanwib@gmail.com>
2022-01-06 00:03:02 +0700

    Desain Halaman Finance

---
+++ app/src/main/java/com/example/plance/FinanceMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.plance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinanceMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finance_main);
+    }
+}
---
+++ app/src/main/java/com/example/plance/FinanceMainEditActivity.java
@@ -0,0 +1,14 @@
+package com.example.plance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinanceMainEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finance_main_edit);
+    }
+}
